2cc389a63e612a0168fc563e1c136640d744d3d9
Kyrotechnics <141758940+Kyrotechnic@users.noreply.github.com>
2024-06-21 13:29:03 -0400

    Add UUID package

--- server/buffer.go
+++ server/buffer.go
@@ -4,4 +4,16 @@ type IBuffer interface {
 	Read(count int) []byte
 	ReadUInt8() uint8
 	ReadInt8() int8
+	ReadUInt16() uint16
+	ReadInt16() int16
+	ReadInt() int32
+	ReadUInt() uint32
+	ReadVarInt() int32
+	ReadFloat() float32
+	ReadDouble() double
+	ReadLong() int64
+	ReadULong() uint64
+	ReadPosition() *Position
+	ReadString() string
+	ReadUUID() uuid.UUID
 }
---
+++ server/datatypes.go
@@ -0,0 +1,105 @@
+package server
+
+type double float64
+
+//Last modified: 6/21/2024
+
+type Vec3 struct {
+	x double
+	y double
+	z double
+}
+
+func (vec *Vec3) GetX() double {
+	return vec.x
+}
+
+func (vec *Vec3) GetY() double {
+	return vec.y
+}
+
+func (vec *Vec3) GetZ() double {
+	return vec.z
+}
+
+func (vec *Vec3) SetX(x double) {
+	vec.x = x
+}
+
+func (vec *Vec3) SetY(y double) {
+	vec.y = y
+}
+
+func (vec *Vec3) SetZ(z double) {
+	vec.z = z
+}
+
+func CreateVec3(x double, y double, z double) *Vec3 {
+	return &Vec3{
+		x: x,
+		y: y,
+		z: z,
+	}
+}
+
+func (vec *Vec3) Add(vec1 Vec3) {
+	vec.x += vec1.x
+	vec.y += vec1.y
+	vec.z += vec1.z
+}
+
+func (vec *Vec3) Sub(vec1 Vec3) {
+	vec.x -= vec1.x
+	vec.y -= vec1.y
+	vec.z -= vec1.z
+}
+
+//Last modified: 6/21/2024
+
+type Position struct {
+	pos   *Vec3
+	yaw   float32
+	pitch float32
+}
+
+func (pos *Position) GetYaw() float32 {
+	return pos.yaw
+}
+
+func (pos *Position) GetPitch() float32 {
+	return pos.pitch
+}
+
+func (pos *Position) SetYaw(f float32) {
+	pos.yaw = f
+}
+
+func (pos *Position) SetPitch(p float32) {
+	pos.pitch = p
+}
+
+func (pos *Position) GetVec3() *Vec3 {
+	return pos.pos
+}
+
+func (pos *Position) SetVec3(vec *Vec3) {
+	pos.pos = vec
+}
+
+func CreatePosition(x double, y double, z double, yaw float32, pitch float32) *Position {
+	return &Position{
+		pos:   CreateVec3(x, y, z),
+		yaw:   yaw,
+		pitch: pitch,
+	}
+}
+
+func CreatePositionVec(vec *Vec3, yaw float32, pitch float32) *Position {
+	return &Position{
+		pos:   vec,
+		yaw:   yaw,
+		pitch: pitch,
+	}
+}
+
+//Now its UUID time
